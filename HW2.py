b392ecf651516df7c1ea89709abc4784866707ce
VetrovaVika <136901382+VetrovaVika@users.noreply.github.com>
2023-09-23 20:11:07 +0700

    Add files via upload

---
+++ HW2.py
@@ -0,0 +1,16 @@
+#Задача 1
+# num = int(input("Задать число "))
+# if num > 0 : print ("num>0")
+# elif num < 0 : print ("num<0")
+
+#Задача 2
+# chislo = int(input("Задать число "))
+# tri = chislo%3
+# if tri == 0 : print("ok")
+# elif tri != 0 : print(":(")
+
+#Задача 3
+# mes = int(input("Укажите номер месяца: "))
+# if mes > 12 : print ("Нет")
+# if mes <= 12 : print ("Да")
+
